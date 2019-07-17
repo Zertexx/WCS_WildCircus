2a85904d3e40f3a2b5698ebb900f66201c8c32ba
Maxime Vasseur <zertouflex@gmail.com>
2019-07-17 10:26:57 +0200

    formulaire + email ok

--- WildCircus/src/Controller/CircusController.php
+++ WildCircus/src/Controller/CircusController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Artist;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Notification\ContactNotification;
 use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CircusController extends AbstractController
@@ -35,10 +40,25 @@ class CircusController extends AbstractController
 
     /**
      * @Route("circus/histoire", name="circus_history")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function history()
+    public function history(Request $request, ContactNotification $notification): Response
     {
-        return $this->render('circus/history.html.twig');
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+            return $this->redirectToRoute('circus_index');
+        }
+            return $this->render('circus/history.html.twig', [
+                'form' => $form->createView(),
+
+
+            ]);
+
 
     }
 }
